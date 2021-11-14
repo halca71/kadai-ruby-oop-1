534fd9cd491be6f87bd7cead27ba511c5091a70f
haruka.yoshida <mindmapping71@gmail.com>
2021-11-14 06:33:13 +0000

    third commit

--- team.rb
+++ team.rb
@@ -13,21 +13,21 @@ class Team
     return win.to_f/(win+lose)
   end
   
-  def show_team_result(name)
+  def show_team_result
     puts "#{@name}の2020年の成績は、#{@win}勝 #{@lose}敗 #{@draw}分、勝率は#{calc_win_rate}です。"
   end
 end
 
-Giants = Team.new('Giants', 67, 45, 8)
-Tigers = Team.new('Tigers', 60, 53, 7)
-Dragons = Team.new('Dragons', 60, 55, 5)
-Baystars = Team.new('Baystars', 56, 58, 6)
-Carp = Team.new('Carp', 52, 56, 12)
-Swallows = Team.new('Swallows', 41, 69, 10)
+giants = Team.new('Giants', 67, 45, 8)
+tigers = Team.new('Tigers', 60, 53, 7)
+dragons = Team.new('Dragons', 60, 55, 5)
+baystars = Team.new('Baystars', 56, 58, 6)
+carp = Team.new('Carp', 52, 56, 12)
+swallows = Team.new('Swallows', 41, 69, 10)
 
-Giants.show_team_result(Giants)
-Tigers.show_team_result(Tigers)
-Dragons.show_team_result(Dragons)
-Baystars.show_team_result(Baystars)
-Carp.show_team_result(Carp)
-Swallows.show_team_result(Swallows)
+giants.show_team_result
+tigers.show_team_result
+dragons.show_team_result
+baystars.show_team_result
+carp.show_team_result
+swallows.show_team_result
